05dc6d51586431926f6ef17adcf10e5d61115b97
cmur85 <39341418+cmurestudillos@users.noreply.github.com>
2021-06-25 08:41:59 +0200

    Actualizar aplicación a versión 12 de Angular

--- src/app/components/inicio/inicio.component.spec.ts
+++ src/app/components/inicio/inicio.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { InicioComponent } from './inicio.component';
 
@@ -6,7 +6,7 @@ describe('InicioComponent', () => {
   let component: InicioComponent;
   let fixture: ComponentFixture<InicioComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ InicioComponent ]
     })
